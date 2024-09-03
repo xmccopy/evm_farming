f5dd41995c195bd5eefc15f685064be2127cd642
techguru0411 <techguru0411@gmail.com>
2024-09-03 16:26:02 +0900

    add: signup&signin&dashboard

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,83 @@
+"use client"
+import Head from "next/head";
+import { useState } from 'react';
+import { RiEyeCloseLine, RiEyeLine } from 'react-icons/ri';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/dashboard');
+  };
+
+
+  return (
+    <div className=" flex flex-col items-center justify-start">
+      <Head>
+        <title>Sign Up</title>
+      </Head>
+
+      <div className=" text-white rounded-lg max-w-md w-full space-y-6">
+        <h1 className="text-3xl font-bold text-center">SIGN IN</h1>
+
+        <div className="m-auto w-64 h-64">
+          {/* Placeholder for the graphic */}
+          <img
+            src="/images/main.png"
+            alt="Sign Up Graphic"
+            className="w-full h-full"
+          />
+        </div>
+
+        <form className="space-y-6">
+          <div>メールアドレス
+            <input
+              type="email"
+              placeholder="メールアドレス"
+              className="w-full px-4 py-2 bg-white border border-white rounded-md text-gray-700 placeholder-gray-400 focus:outline-none focus:border-green-400"
+            />
+          </div>
+
+          <div className="relative">
+            <div>パスワード</div>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="パスワード"
+              className="w-full px-4 py-2 bg-white border border-white rounded-md text-gray-700 placeholder-gray-400 focus:outline-none focus:border-green-400"
+            />
+            <br />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1 transform -translate-y-1/2 text-gray-400" style={{ marginTop: '40px' }}
+            >
+              {showPassword ? <RiEyeLine /> : <RiEyeCloseLine />}
+            </button>
+          </div>
+
+          {/* <div>
+            アクティベーションコード
+
+            <input
+              type="text"
+              placeholder="アクティベーションコード"
+              className="w-full px-4 py-2 bg-white border border-white rounded-md text-gray-700 placeholder-gray-400 focus:outline-none focus:border-green-400"
+            />
+          </div> */}
+          <button onClick={handleClick} className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">
+            サインイン
+          </button>
+        </form>
+
+        <div className="text-center">
+          <a href="/signup" className="text-[#D2AB67] underline text-[16px]">
+            新規登録はこちら
+          </a>
+        </div>
+      </div>
+    </div >
+  );
+}
---
+++ app/(page)/dashboard/page.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div className=" flex flex-col items-center justify-start text-white">
+            <Head>
+                <title>Dashboard</title>
+            </Head>
+
+            <div className="m-auto w-64 h-64">
+                {/* Placeholder for the graphic */}
+                <img
+                    src="/images/main.png"
+                    alt="Sign Up Graphic"
+                    className="w-full h-full"
+                />
+            </div>
+
+
+            <h1 className="text-3xl font-bold mb-8">EVM ファーミング</h1>
+
+            <button className="w-64 py-3 mb-4 text-lg font-semibold bg-[linear-gradient(160deg,_#665DCD_0%,_#5FA4E6_44.76%,_#D2AB67_100%)] hover:bg-[linear-gradient(200deg,_#665DCD_0%,_#5FA4E6_44.76%,_#D2AB67_100%)] rounded-[30px] shadow-md transition-all duration-1000 ease-in-out">
+                ライセンス購入
+            </button>
+
+            <p className="mb-4">フリーミアムウォレット</p>
+            <p className="mb-8 text-center">フリーミアムウォレットはまだありません。<br />まずは作成しましょう。</p>
+
+            <button className="w-64 py-3 text-lg font-semibold bg-[linear-gradient(107.21deg,_#9348A2_24.43%,_#2954A3_68.95%)] hover:bg-[linear-gradient(200.21deg,_#9348A2_24.43%,_#2954A3_68.95%)] rounded-[30px] shadow-md transition-all duration-1000 ease-in-out hover:ease-out">
+                + ウォレットをインポート
+            </button>
+        </div>
+    );
+}
